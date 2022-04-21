5af28c11eb6f11bf6781061add00858c83be17f5
Enrique Marina <eherraiz@myuax.com>
2022-04-21 15:15:53 +0200

    acabado

--- Ejercicios/Ejercicio_1.py
+++ Ejercicios/Ejercicio_1.py
@@ -5,8 +5,9 @@ class Bloque:
         # Por defecto, un bloque no contiene ninguna instrucción.
         self.instrucciones = []
 
-    def agregarInstruction(self, instruccion):
-        self.instrucciones.append(instruccion)
+    def agregarInstruccion(self, instruccion):
+        for i in instruccion:
+            self.instrucciones.append(i)
 
 class Si:
     # Representa una instrucción 'if'. 'condicion' es una cadena
@@ -20,6 +21,9 @@ class Si:
         self.entonces = entonces
         self.si_no = si_no
 
+    def lista(self):
+        return [self.condicion, self.entonces, self.si_no]
+
 class MientrasQue:
     # Representa una instrucción 'while'.
     # 'condicion' es una cadena que contiene el valor evaluado
@@ -29,16 +33,29 @@ class MientrasQue:
         self.condicion = condicion
         self.bloque = bloque
 
+    def si_se_cumple(self):
+        if self.condicion == bool(self.bloque.instrucciones[0]):
+            return self.bloque.instrucciones[1]
+        else:
+            return self.bloque.instrucciones[2]
+
+
 class Mostrar:
     # Una instrucción para mostrar un mensaje
     # en salida estándar.
     def __init__(self, mensaje):
         self.mensaje = mensaje
 
+    def __str__(self):
+        return self.mensaje
+
+
 
 mostrar_ok = Mostrar('"OK"')
 mostrar_ko = Mostrar('"KO"')
 alternativa = Si("2 + 2 == 4", mostrar_ok, mostrar_ko)
 bloque_alternativa = Bloque()
-bloque_alternativa.agregarInstruccion(alternativa)
+bloque_alternativa.agregarInstruccion(alternativa.lista())
 bucle = MientrasQue(True, bloque_alternativa)
+print(bucle.si_se_cumple())
+
